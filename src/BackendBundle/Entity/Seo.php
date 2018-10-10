b120ce2a773d6e416e197fdc3fcd760bfa6d7e5b
DMK <dmkshift@gmail.com>
2018-10-10 11:50:27 +0300

    start

---
+++ src/BackendBundle/Entity/Seo.php
@@ -0,0 +1,136 @@
+<?php
+namespace BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="seo_table")
+ * @Vich\Uploadable
+ */
+class Seo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $page;
+
+    /**
+     * @Gedmo\Slug(fields={"page"},unique=true,separator="-")
+     * @ORM\Column(name="slug", length=255, unique=true)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $keywords;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKeywords()
+    {
+        return $this->keywords;
+    }
+
+    /**
+     * @param mixed $keywords
+     */
+    public function setKeywords($keywords)
+    {
+        $this->keywords = $keywords;
+    }
+
+    /**
+     * @param mixed $page
+     */
+    public function setPage($page)
+    {
+        $this->page = $page;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param mixed $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+}
---
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FrontendBundle\Controller;
+
+use BackendBundle\Entity\Project;
+use BackendBundle\Entity\Responsibility_home;
+use BackendBundle\Entity\RiskManagement;
+use BackendBundle\Entity\Trading;
+use FrontendBundle\Form\Type\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use BackendBundle\Entity\ContactForm;
+use BackendBundle\Entity\Contacts;
+use BackendBundle\Entity\Seo;
+use BackendBundle\Entity\About;
+
+use BackendBundle\Entity\Responsibility;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+//    /**
+//     * @Route("/", name="homepage")
+//     */
+//    public function indexAction()
+//    {
+//        $em = $this->getDoctrine()->getManager();
+//
+//
+//        return $this->render();
+//    }
+}
